fbc544c2387b1f32769ffb89fdc0f58b62ccfbac
Davidtricolor2021 <92552378+Davidtricolor2021@users.noreply.github.com>
2022-05-03 12:50:48 -0300

    Create config.php

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+
+//conexao ao banco de dados (MySQL)
+define("BD_SERVIDOR", "127.0.0.1");
+define("BD_USUARIO", "sistematarefas");
+define("BD_SENHA", "sistematarefas");
+define("BD_BANCO", "tarefas");
+
+//E-mail para notificação
+define("EMAIL_NOTIFICACAO", "testesdavidphp@gmail.com");
