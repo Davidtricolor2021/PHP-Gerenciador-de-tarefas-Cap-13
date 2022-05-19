a41be507d5cf9cdee157ad3107cdd41bc137dd25
Davidtricolor2021 <92552378+Davidtricolor2021@users.noreply.github.com>
2022-05-19 12:18:31 -0300

    Update template.php

--- template.php
+++ template.php
@@ -1,16 +1,18 @@
 <html>
     <head>
-        <meta charset="utf-8"/>
+        <meta charset="utf-8" />
         <title>Gerenciador de Tarefas</title>
-        <link rel="stylesheet" href="tarefas.css" type="text/css"/>
+        <link rel="stylesheet" href="tarefas.css" type="text/css" />
     </head>
     <body>
-        <h1>Gerenciador de Tarefas</h1>
-        
-        <?php include ('formulario.php'); ?>
+        <div id="bloco_principal">
+            <h1>Gerenciador de Tarefas</h1>
 
-        <?php if ($exibir_tabela) : ?>
-            <?php include ('tabela.php'); ?>
-        <?php endif; ?>
+            <?php include('formulario.php'); ?>
+
+            <?php if ($exibir_tabela) : ?>
+                <?php include('tabela.php'); ?>
+            <?php endif; ?>
+        </div>
     </body>
 </html>
