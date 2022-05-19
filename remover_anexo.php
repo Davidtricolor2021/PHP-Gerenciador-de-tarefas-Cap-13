42ce286fc9bde89a91b13101327712e2002d7047
Davidtricolor2021 <92552378+Davidtricolor2021@users.noreply.github.com>
2022-05-19 12:16:59 -0300

    Update remover_anexo.php

--- remover_anexo.php
+++ remover_anexo.php
@@ -2,16 +2,12 @@
 
 require "config.php";
 require "banco.php";
+require "classes/Anexo.php";
+require "classes/RepositorioTarefas.php";
 
-$anexo = buscar_anexo($conexao, $_GET['id']);
+$repositorio_tarefas = new RepositorioTarefas($mysqli);
+$anexo = $repositorio_tarefas->buscar_anexo($_GET['id']);
+$repositorio_tarefas->remover_anexo($anexo->getId());
+unlink('anexos/' . $anexo->getArquivo());
 
-if (! is_array($anexo)) {
-    http_response_code(404);
-    echo "Anexo não encontrado.";
-    die();
-}
-
-remover_anexo($conexao, $anexo['id']);
-unlink('anexos/' . $anexo['arquivo']);
-
-header('Location: tarefa.php?id=' . $anexo['tarefa_id']);
+header('Location: tarefa.php?id=' . $anexo->getTarefaId());
