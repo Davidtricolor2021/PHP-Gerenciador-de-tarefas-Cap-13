aa4c64c851d95804532c305618fb516b22a75111
Davidtricolor2021 <92552378+Davidtricolor2021@users.noreply.github.com>
2022-05-19 12:19:04 -0300

    Update template_tarefa.php

--- template_tarefa.php
+++ template_tarefa.php
@@ -5,46 +5,33 @@
         <link rel="stylesheet" href="tarefas.css" type="text/css" />
     </head>
     <body>
-        <div="bloco_principal">
-            <h1>Tarefa: <?php echo $tarefa['nome']; ?></h1>
-            <p>
-                <a href="tarefas.php">
-                    Voltar para a lista de tarefas
-                </a>
-            </p>
-            <p>
-                <strong>Concluída:</strong> <?php echo traduz_concluida($tarefa['concluida']); ?>
-            </p>
-            <p>
-                <strong>Descrição:</strong> <?php echo nl2br($tarefa['descricao']); ?>
-            </p>
-            <p>
-                <strong>Prazo:</strong> <?php echo traduz_data_para_exibir($tarefa['prazo']); ?>
-            </p>
-            <p>
-                <strong>Prioridade:</strong> <?php echo traduz_prioridade($tarefa['prioridade']); ?>
-            </p>
+        <div id="bloco_principal">
+            <h1>Tarefa: <?php echo $tarefa->getNome(); ?></h1>
 
-            <h2>Anexos<h2>
+            <p><a href="tarefas.php">Voltar para a lista de tarefas</a>.</p>
+
+            <p><strong>Concluída:</strong> <?php echo traduz_concluida($tarefa->getConcluida()); ?></p>
+            <p><strong>Descrição:</strong> <?php echo nl2br($tarefa->getDescricao()); ?></p>
+            <p><strong>Prazo:</strong> <?php echo traduz_data_para_exibir($tarefa->getPrazo()); ?></p>
+            <p><strong>Prioridade:</strong> <?php echo traduz_prioridade($tarefa->getPrioridade()); ?></p>
+
+            <h2>Anexos</h2>
             <!-- lista de anexos -->
-            <?php if (count($anexos) > 0) : ?>
+            <?php if (count($tarefa->getAnexos()) > 0) : ?>
                 <table>
                     <tr>
                         <th>Arquivo</th>
                         <th>Opções</th>
                     </tr>
-
-                    <?php foreach ($anexos as $anexo) : ?>
+                    <?php foreach ($tarefa->getAnexos() as $anexo) : ?>
                         <tr>
-                            <td><?php echo $anexo['nome']; ?></td>
+                            <td><?php echo $anexo->getNome(); ?></td>
                             <td>
-                                <a href="anexos/<?php echo $anexo['arquivo']; ?>"> Download </a>
-                            </td>
-                                <a href="remover_anexo.php?id=<?php echo $anexo['id']; ?>"> Remover </a>
+                                <a href="anexos/<?php echo $anexo->getArquivo(); ?>">Download</a>
+                                <a href="remover_anexo.php?id=<?php echo $anexo->getId(); ?>">Remover</a>
                             </td>   
                         </tr>
                     <?php endforeach; ?>
-
                 </table>
             <?php else : ?>
                 <p>Não há anexos para esta tarefa.</p>
@@ -54,20 +41,14 @@
             <form action="" method="post" enctype="multipart/form-data">
                 <fieldset>
                     <legend>Novo anexo</legend>
-
-                    <input type="hidden" name="tarefa_id" value="<?php echo $tarefa['id']; ?>" />
-
+                    <input type="hidden" name="tarefa_id" value="<?php echo $tarefa->getId(); ?>" />
                     <label>
-                        <?php if ($tem_erros && array_key_exists('anexo', $erros_validacao) ):?>
-                            <span class="erro">
-                                <?php echo $erros_validacao['anexo']; ?>
-                            </span>
+                        <?php if ($tem_erros && isset($erros_validacao['anexo'])) : ?>
+                            <span class="erro"><?php echo $erros_validacao['anexo']; ?></span>
                         <?php endif; ?>
-
                         <input type="file" name="anexo" />
                     </label>
-
-                    <input type="submit" value="Cadastrar" />
+                    <input type="submit" value="Cadastrar" class="botao" />
                 </fieldset>
             </form>
         </div>
